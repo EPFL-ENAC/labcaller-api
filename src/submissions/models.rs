05aeaaa68983989edf257d0cb43955ad52830ab9
Evan Thomas <evan.thomas@epfl.ch>
2024-10-06 12:39:04 +0200

    Use double option for update payload

--- src/submissions/models.rs
+++ src/submissions/models.rs
@@ -1,7 +1,8 @@
-use chrono::NaiveDate;
+use super::db::ActiveModel;
 use chrono::NaiveDateTime;
-use sea_orm::FromQueryResult;
-use serde::Serialize;
+use sea_orm::{DeriveIntoActiveModel, FromQueryResult, InsertResult, NotSet, Set};
+use serde::{Deserialize, Deserializer, Serialize};
+use serde_with::rust::double_option;
 use utoipa::ToSchema;
 use uuid::Uuid;
 
@@ -12,11 +13,92 @@ pub struct Submission {
     processing_has_started: bool,
     processing_success: bool,
     comment: Option<String>,
-    created_on: Option<NaiveDate>,
+    created_on: NaiveDateTime,
     last_updated: NaiveDateTime,
 }
 
 impl From<super::db::Model> for Submission {
+    fn from(model: super::db::Model) -> Self {
+        Self {
+            id: model.id,
+            name: model.name,
+            processing_has_started: model.processing_has_started,
+            processing_success: model.processing_success,
+            comment: model.comment,
+            created_on: model.created_on,
+            last_updated: model.last_updated,
+        }
+    }
+}
+
+#[derive(ToSchema, Deserialize, Serialize, DeriveIntoActiveModel)]
+pub struct SubmissionCreate {
+    pub name: String,
+    pub comment: Option<String>,
+}
+
+#[derive(ToSchema, Deserialize)]
+pub struct SubmissionUpdate {
+    #[serde(
+        default,                                    // <- important for deserialization
+        skip_serializing_if = "Option::is_none",    // <- important for serialization
+        with = "::serde_with::rust::double_option",
+    )]
+    pub name: Option<Option<String>>,
+    #[serde(
+        default,                                    // <- important for deserialization
+        skip_serializing_if = "Option::is_none",    // <- important for serialization
+        with = "::serde_with::rust::double_option",
+    )]
+    pub comment: Option<Option<String>>,
+}
+
+impl From<SubmissionUpdate> for ActiveModel {
+    fn from(update: SubmissionUpdate) -> Self {
+        // If the field is Some(None), update the field to None, if None,
+        // do not update the field (double option)
+        Self {
+            name: match update.name {
+                Some(Some(name)) => Set(name),
+                Some(None) => NotSet,
+                None => NotSet,
+            },
+            comment: match update.comment {
+                Some(Some(comment)) => Set(Some(comment)),
+                Some(None) => NotSet,
+                None => NotSet,
+            },
+            last_updated: Set(chrono::Utc::now().naive_utc()),
+            id: NotSet,
+            processing_has_started: NotSet,
+            processing_success: NotSet,
+            created_on: NotSet,
+        }
+    }
+}
+
+// // Custom deserialization logic for double option fields
+// fn deserialize_double_option<'de, D>(deserializer: D) -> Result<Option<Option<String>>, D::Error>
+// where
+//     D: Deserializer<'de>,
+// {
+//     let opt = double_option::deserialize(deserializer)?;
+
+//     // Convert Some(None) to None to exclude from deserialization
+//     Ok(opt.filter(|inner| inner.is_some()))
+// }
+#[derive(ToSchema, Serialize, FromQueryResult)]
+pub struct SubmissionReadOne {
+    id: Uuid,
+    name: String,
+    processing_has_started: bool,
+    processing_success: bool,
+    comment: Option<String>,
+    created_on: NaiveDateTime,
+    last_updated: NaiveDateTime,
+}
+
+impl From<super::db::Model> for SubmissionReadOne {
     fn from(model: super::db::Model) -> Self {
         Self {
             id: model.id,
--- src/submissions/views.rs
+++ src/submissions/views.rs
@@ -2,17 +2,22 @@ use crate::common::filter::{apply_filters, parse_range};
 use crate::common::models::FilterOptions;
 use crate::common::pagination::calculate_content_range;
 use crate::common::sort::generic_sort;
+use axum::http::{response, StatusCode};
 use axum::response::IntoResponse;
 use axum::{
-    extract::{Query, State},
+    debug_handler,
+    extract::{Path, Query, State},
     routing, Json, Router,
 };
-use sea_orm::EntityTrait;
-use sea_orm::{query::*, DatabaseConnection};
+use sea_orm::{query::*, DatabaseConnection, DbErr, IntoActiveModel, SqlErr, SqlxError};
+use sea_orm::{EntityTrait, RuntimeErr};
+use std::borrow::Cow;
+use uuid::Uuid;
 
 pub fn router(db: DatabaseConnection) -> Router {
     Router::new()
-        .route("/", routing::get(get_all))
+        .route("/", routing::get(get_all).post(create_one))
+        .route("/:id", routing::get(get_one).put(update_one))
         .with_state(db)
 }
 
@@ -70,4 +75,100 @@ pub async fn get_all(
     let headers = calculate_content_range(offset, limit, total_count, RESOURCE_NAME);
 
     (headers, Json(response_objs))
+}
+
+pub async fn create_one(
+    State(db): State<DatabaseConnection>,
+    Json(payload): Json<super::models::SubmissionCreate>,
+) -> Result<(StatusCode, Json<super::models::SubmissionReadOne>), (StatusCode, Json<String>)> {
+    let new_obj = super::db::Model {
+        id: uuid::Uuid::new_v4(),
+        name: payload.name,
+        processing_has_started: false,
+        processing_success: false,
+        comment: payload.comment,
+        created_on: chrono::Utc::now().naive_utc(),
+        last_updated: chrono::Utc::now().naive_utc(),
+    }
+    .into_active_model();
+
+    match super::db::Entity::insert(new_obj).exec(&db).await {
+        Ok(insert_result) => {
+            let response_obj: super::models::SubmissionReadOne =
+                super::db::Entity::find_by_id(insert_result.last_insert_id)
+                    .one(&db)
+                    .await
+                    .expect("Failed to find object")
+                    .unwrap()
+                    .into();
+
+            Ok((StatusCode::CREATED, Json(response_obj)))
+        }
+        Err(err) => match err.sql_err() {
+            Some(SqlErr::UniqueConstraintViolation(_)) => {
+                Err((StatusCode::CONFLICT, Json("Duplicate entry".to_string())))
+            }
+            Some(_) => Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json("Error adding object".to_string()),
+            )),
+            _ => Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json("Server error".to_string()),
+            )),
+        },
+    }
+}
+
+#[utoipa::path(
+    get,
+    path = format!("/api/{}/{{id}}", RESOURCE_NAME),
+    responses((status = OK, body = super::models::SubmissionReadOne))
+)]
+#[debug_handler]
+pub async fn get_one(
+    State(db): State<DatabaseConnection>,
+    Path(id): Path<Uuid>,
+) -> Result<Json<super::models::SubmissionReadOne>, (StatusCode, Json<String>)> {
+    let obj = super::db::Entity::find_by_id(id)
+        .one(&db)
+        .await
+        .map_err(|_| (StatusCode::NOT_FOUND, Json("Not found".to_string())))?
+        .ok_or((StatusCode::NOT_FOUND, Json("Not found".to_string())))?;
+
+    Ok(Json(obj.into()))
+}
+
+#[utoipa::path(
+    put,
+    path = format!("/api/{}/{{id}}", RESOURCE_NAME),
+    responses((status = OK, body = super::models::SubmissionReadOne))
+)]
+pub async fn update_one(
+    State(db): State<DatabaseConnection>,
+    Path(id): Path<Uuid>,
+    Json(payload): Json<super::models::SubmissionUpdate>,
+) -> impl IntoResponse {
+    // ) -> Result<Json<super::models::SubmissionReadOne>, (StatusCode, Json<String>)> {
+    println!("Update one {:?} {:?}", payload.name, payload.comment);
+    let update_object: super::db::ActiveModel = payload.into();
+
+    let obj = super::db::Entity::find_by_id(id)
+        .one(&db)
+        .await
+        .unwrap()
+        .expect("Failed to find object");
+
+    // Update obj with the new values from update_object
+
+    // .unwrap()
+    // .unwrap();
+
+    // Update the object
+    // obj.update(&db).await;
+
+    println!("Update one {:?}", obj);
+    println!("Update object {:?}", update_object);
+
+    ()
 }
